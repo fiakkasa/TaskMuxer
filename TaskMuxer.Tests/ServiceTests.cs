c6551c58d61a5b0f90bfa262ba8604f6794de21e
Adam <fiakkasa@live.com>
2023-08-28 22:43:06 +0300

    UT Amends

--- TaskMuxer.Tests/ServiceTests.cs
+++ TaskMuxer.Tests/ServiceTests.cs
@@ -17,7 +17,7 @@ public class ServiceTests
         public SampleService(ITaskMultiplexer taskMultiplexer, SampleDataContext dbContext) =>
             (_taskMultiplexer, _dbContext) = (taskMultiplexer, dbContext);
 
-        public async Task<SampleEntity?> GetByIdWithSemaphore(int id, CancellationToken cancellationToken = default) =>
+        public async Task<SampleEntity?> GetById(int id, CancellationToken cancellationToken = default) =>
             await _taskMultiplexer.AddTask(
                 "get_by_id_with_semaphore",
                 async ct =>
@@ -36,34 +36,7 @@ public class ServiceTests
                 cancellationToken
             );
 
-        public async Task<SampleEntity?> GetById(int id, CancellationToken cancellationToken = default) =>
-            await _taskMultiplexer.AddTask(
-                "get_by_id",
-                async ct => await _dbContext.Samples.FirstOrDefaultAsync(x => x.Id == id, cancellationToken),
-                cancellationToken
-            );
-
-        public async Task<List<SampleEntity>?> GetAll(CancellationToken cancellationToken = default) =>
-            await _taskMultiplexer.AddTask(
-                "get_all",
-                async ct => await _dbContext.Samples.ToListAsync(cancellationToken),
-                cancellationToken
-            );
-
         public async Task<SampleEntity?> Add(SampleEntity obj, CancellationToken cancellationToken = default) =>
-            await _taskMultiplexer.AddTask(
-                "add",
-                async ct =>
-                {
-                    _dbContext.Add(obj);
-                    await _dbContext.SaveChangesAsync(ct);
-
-                    return obj;
-                },
-                cancellationToken
-            );
-
-        public async Task<SampleEntity?> AddWithSemaphore(SampleEntity obj, CancellationToken cancellationToken = default) =>
             await _taskMultiplexer.AddTask(
                 "add_with_semaphore",
                 async ct =>
@@ -138,56 +111,12 @@ public class ServiceTests
     }
 
     [Fact]
-    public async Task Service_Get_EF()
-    {
-        var service = new SampleService(
-            new InstanceTaskMultiplexer(),
-            EFTestingUtils.GetDbCtx(
-                Enumerable.Range(1, 10)
-                    .Select(v => new SampleEntity
-                    {
-                        Id = v,
-                        Text = "Text " + v
-                    })
-                    .ToArray()
-            )
-        );
-
-        var results = await Task.WhenAll(Enumerable.Range(0, 10).Select((_, __) => service.Samples.ToListAsync()));
-
-        Assert.Equal(10, results.Length);
-        Assert.Equal(10, results.SelectMany(x => x!).Distinct().Count());
-    }
-
-    [Fact]
-    public async Task Service_GetAll_EF()
-    {
-        var service = new SampleService(
-            new InstanceTaskMultiplexer(),
-            EFTestingUtils.GetDbCtx(
-                Enumerable.Range(1, 10)
-                    .Select(v => new SampleEntity
-                    {
-                        Id = v,
-                        Text = "Text " + v
-                    })
-                    .ToArray()
-            )
-        );
-
-        var results = await Task.WhenAll(Enumerable.Range(0, 10).Select((_, __) => service.GetAll()));
-
-        Assert.Equal(10, results.Length);
-        Assert.Equal(10, results.SelectMany(x => x!).Distinct().Count());
-    }
-
-    [Fact]
     public async Task Service_Add_EF()
     {
         var service = new SampleService(new InstanceTaskMultiplexer(), EFTestingUtils.GetDbCtx());
 
         var results = await Task.WhenAll(
-            Enumerable.Range(0, 2)
+            Enumerable.Range(0, 10)
                 .Select((_, __) => service.Add(
                     new()
                     {
@@ -205,14 +134,20 @@ public class ServiceTests
         var service = new SampleService(new InstanceTaskMultiplexer(), EFTestingUtils.GetDbCtx());
 
         await Task.WhenAll(
-            service.GetByIdWithSemaphore(1),
-            service.AddWithSemaphore(
+            service.GetById(1),
+            service.Add(
                 new()
                 {
                     Text = "Random " + Random.Shared.Next()
                 }
             ),
-            service.GetByIdWithSemaphore(1)
+            service.GetById(1),
+            service.Add(
+                new()
+                {
+                    Text = "Random " + Random.Shared.Next()
+                }
+            )
         );
 
         Assert.Single(service.Samples);
